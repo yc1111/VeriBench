5df64cdd77d3788c7dec3c52dc517137d99c813d
yc1111 <yuecong@gmail.com>
2024-02-29 15:26:27 +0000

    Fix glassdb dockerfile

--- exps/setup.sh
+++ exps/setup.sh
@@ -8,9 +8,11 @@ for host in `cat ${exp_dir}/clients ${exp_dir}/workers`
 do
   echo $host
   rsync -a $dir/VeriBench $host:$dir
-  #ssh $host "docker pull yc1111/veribench:glassdb"
-  #ssh $host "docker pull yc1111/veribench:ledgerdb"
-  #ssh $host "docker pull yc1111/veribench:sqlledger"
-  #ssh $host "docker pull  yc1111/veribench:qldb"
+  ssh $host "docker pull yc1111/veribench:glassdb"
+  ssh $host "docker pull yc1111/veribench:ledgerdb"
+  ssh $host "docker pull yc1111/veribench:sqlledger"
+  ssh $host "docker pull yc1111/veribench:qldb"
+  ssh $host "docker pull yc1111/veribench:ccf"
+  ssh $host "docker pull yc1111/veribench:merkle2"
 done
 
