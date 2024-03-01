88a9328668d887a7fe18e5d50c0ca7697d6ce7aa
yc1111 <yuecong@gmail.com>
2024-03-01 04:10:02 +0000

    fix ccf stop_server.sh

--- dbadapter/ccf/conf/stop_server.sh
+++ dbadapter/ccf/conf/stop_server.sh
@@ -1,5 +1,5 @@
 . conf.sh
 
 echo "stopping server"
-
-curl -X GET "https://10.10.10.237:8080/app/commit" --cacert $db_config_dir/external/service_cert.pem --key $db_config_dir/script/user0_privk.pem --cert $db_config_dir/script/user0_cert.pem  >> $db_config_dir/tid
+primary=`sed -n "1p" $exp_dir/workers`
+curl -X GET "https://${primary}/app/commit" --cacert $db_config_dir/external/service_cert.pem --key $db_config_dir/script/user0_privk.pem --cert $db_config_dir/script/user0_cert.pem  >> $db_config_dir/tid
